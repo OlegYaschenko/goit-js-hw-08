b6cead0c60b13cc14d233ce913903462801114d4
OlegYaschenko <olegyaschenko@gmail.com>
2021-12-24 18:05:43 +0200

    2й блок

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+}
+
+const createMarkupEl = galleryItems.map(({ original, preview, description}) =>
+  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`)
+.join('');
+
+refs.gallery.innerHTML = createMarkupEl;
+
+new SimpleLightbox('.gallery a', { 
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const savedSettings = localStorage.getItem("videoplayer-current-time");
+const parsedSettings = JSON.parse(savedSettings);
+
+
+player.setCurrentTime(parsedSettings);
+
+player.on('timeupdate', throttle(saveCurrentTimeVideo, 1000));
+
+function saveCurrentTimeVideo() { 
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+  });
+};
+
 
